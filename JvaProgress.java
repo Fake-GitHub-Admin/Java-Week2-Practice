aa8ce6473fe059299d9545d6d950a06797a5c332
Fake-GitHub-Admin <81976991+Fake-GitHub-Admin@users.noreply.github.com>
2022-03-27 19:34:38 -0400

    Add files via upload

---
+++ JvaProgress.java
@@ -0,0 +1,25 @@
+public class JvaProgress {
+   public static void main(String[] args) {
+      System.out.println("Java Code Progress as of 3/27/2022");
+      String[] violatorsList = {};
+      String[] nonviolatorsList = {};
+      
+      
+      
+
+      
+      
+      
+      //This two are going to help me print out what is in those two arrays at line 2 and line 3
+      //Side Note: Combined for loop is for multidimensional arrays, don't forget to seperate them
+      for (var i = 0; i < violatorsList.length; i++) 
+         {
+            System.out.println(i);
+         }
+      
+      for (var v = 0; v < nonviolatorsList.length; v++)
+         {
+            System.out.println(v);
+         }
+   }
+} 

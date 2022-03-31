d0b24095753a5ebc901caad3b38204f6a0be2b22
Fake-GitHub-Admin <81976991+Fake-GitHub-Admin@users.noreply.github.com>
2022-03-30 23:31:51 -0400

    Add files via upload
    3-30-2022

--- JvaProgress.java
+++ JvaProgress.java
@@ -5,7 +5,7 @@ public class JvaProgress {
    //all methods are automatically public btw
    static void bothLists() 
       {
-         System.out.println("Java Code Progress as of 3/28/2022");
+         System.out.println("Java Code Progress as of 3/30/2022");
          String[] violatorsList = {};
          String[] nonviolatorsList = {};
       
@@ -28,11 +28,11 @@ public class JvaProgress {
       if (YesOrNoForCriminal == "Y")
       {
          
-         System.out.println("Suspect " + name + " " + gender + " " + age + " tested Positive on test for Criminals");
+         System.out.println("Suspect " + name + " " + gender + " "  + "(age: " + age + ") tested Positive on test for Criminals");
       }
       else if (YesOrNoForCriminal == "N")
       {
-         System.out.println("Suspect " + name + " " + gender + " " + age + " tested Negative on test for Criminals");
+         System.out.println("Suspect " + name + " " + gender + " " + "(age:" + age + ") tested Negative on test for Criminals");
       }
       else 
       {
@@ -44,6 +44,7 @@ public class JvaProgress {
       //this will be the new main where the code will focus upon first 
       public static void main (String args[]) {
       bothLists(); //Call the method to activate those empty arrays
+      System.out.println("This is in bulleted list format of everyone");
       theList("Sarah Lee","Female",40,"Y"); //Call the conditional method 
       theList("Rose Ana","Female",19,"N");
       theList("John Blue","Male",27,"Y");

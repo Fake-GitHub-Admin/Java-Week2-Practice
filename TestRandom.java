f5a0d1259a61a71dbe3f4c840fb0e9ecc42c38c5
Fake-GitHub-Admin <81976991+Fake-GitHub-Admin@users.noreply.github.com>
2022-04-06 23:31:25 -0400

    Add files via upload

--- TestRandom.java
+++ TestRandom.java
@@ -1,72 +1,62 @@
 import java.util.ArrayList;
 public class TestRandom {
-   //convert this from main task to a side task
-   //all methods are automatically public btw
    static void bothLists() 
       {
-         System.out.println("Java Code Progress as of 3/31/2022");
-         //String[] violatorsList = {};
-         String[] nonviolatorsList = {};
-         
-      
-      
-      //This two are going to help me print out what is in those two arrays at line 2 and line 3
-      //Side Note: Combined for loop is for multidimensional arrays, don't forget to seperate them
-      /* for (var i = 0; i < violatorsList.size(); i++) 
-         {
-            System.out.println(i);
-         }
-      */
-      for (var v = 0; v < nonviolatorsList.length; v++)
-         {
-            System.out.println(v);
-         }
-      
-   }
+         System.out.println("Java Code Progress as of 4/6/2022");   
+      }
    static void theList(String name, String gender, int age, String YesOrNoForCriminal) 
    {
       ArrayList<String> violatorsList = new ArrayList<String>();
       ArrayList<String> nonviolatorsList = new ArrayList<String>();
+      ArrayList<String> errorList = new ArrayList<String>();
       if (YesOrNoForCriminal == "Y")
       {
          violatorsList.add(name);
          //System.out.println("Suspect " + name + " " + gender + " "  + "(age: " + age + ") tested Positive on test for Criminals");
-         for (String i: violatorsList) 
+         
+         
+   //Ommitted For Loop that was causing the print inaccuracy on 4/5/2022
+       /*  for (String i: violatorsList) 
          {
             System.out.println("Print out all suspects in the ArrayList:");
             System.out.println(i + " ");
          }
- 
+       */
       }
       else if (YesOrNoForCriminal == "N")
       {
          nonviolatorsList.add(name);
          //System.out.println("Suspect " + name + " " + gender + " " + "(age:" + age + ") tested Negative on test for Criminals");
-           for (String i: nonviolatorsList) 
+         
+    //Ommitted For Loop that was causing the print inaccuracy on 4/5/2022
+          /* for (String i: nonviolatorsList) 
          {
             System.out.println("Print out all nonsuspects in the ArrayList:");
             System.out.println(i + " ");
          }
+         */
       }
       else 
       {
-         
+         errorList.add(name);
          //System.out.println("Error, Please insert the appropiate response Y or N");
       }   
+      
+      System.out.println(violatorsList);
+      System.out.println(nonviolatorsList);
+      System.out.println(errorList);
 
    }
 
-      //this will be the new main where the code will focus upon first 
+      
       public static void main (String args[]) {
-      bothLists(); //Call the method to activate those empty arrays
-      //System.out.println("This is in bulleted list format of everyone");
+      bothLists(); //This method useless now, but nice to have though
       theList("Sarah Lee","Female",40,"Y"); //Call the conditional method 
       theList("Rose Ana","Female",19,"N");
       theList("John Blue","Male",27,"Y");
       theList("Eberhault Wick","Male",40,"B");
-
       
-      
+    
       
 
       }
